561fc9282790e8ea16f7a3cb49f419aec8f3bc54
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2025-07-23 21:20:13 +0500

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 function Navbar(){
     return(
-        <div className="navbar">Hello world</div>
+        <div className="navbar">Hello world1224232</div>
     )
 }
 export default Navbar
